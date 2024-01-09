859d1e8c00bff6a24bee3b91f7292876e657a952
kimgoat <kimgoatlove@gmail.com>
2024-01-09 14:30:03 +0900

    예산
    반복문, 조건문, 카운터

---
+++ 예산/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            sum+=d[i];
+            answer++;
+            if(sum > budget){
+                return i;
+            }
+        }
+        
+        return answer;
+    }
+}
